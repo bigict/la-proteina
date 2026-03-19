3e1975bde9142c5923ad8b20620e7920cb831259
chungongyu <chungongyu@gmail.com>
2026-03-19 17:37:03 +0800

    fix: add pseudo linker to multimer

--- script_utils/pdb_preprocess.py
+++ script_utils/pdb_preprocess.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import itertools
 import pathlib
 import random
-from typing import Optional, Union
+from typing import List, Optional, Union
 
 import pandas as pd
 from tqdm.auto import tqdm
@@ -18,20 +18,43 @@ def read_pdb(
     pdb_code: Optional[str] = None,
     uniprot_id: Optional[str] = None,
     model_index: int = 1,
-):
+) -> pd.DataFrame:
     df = read_pdb_to_dataframe(
         path=path,
         pdb_code=pdb_code,
         uniprot_id=uniprot_id,
         model_index=model_index
     )
+    df = df.loc[df["record_name"] == "ATOM"]
     df = sort_dataframe(df)
 
     return df
 
 
-def save_pdb(path: pathlib.Path, df: pd.DataFrame):
-    return save_pdb_df_to_pdb(df, path)
+def save_pdb(path: pathlib.Path, df: pd.DataFrame) -> None:
+    save_pdb_df_to_pdb(df, path)
+
+
+def rearrange_pdb(
+    df: pd.DataFrame, chain_list: List[str], pseudo_linker_length: int = 128
+) -> pd.DataFrame:
+    chains = sorted(
+        df.groupby(by=["chain_id"]), key=lambda g: chain_list.index(g[0][0])
+    )
+
+    residue_number_last = 0
+    for i, (_, g) in enumerate(chains):
+        residue_number_min = g["residue_number"].min()
+        residue_number_offset = (
+            residue_number_last + i * pseudo_linker_length
+        )
+        g["residue_number"] = g["residue_number"].apply(
+            lambda x: x - residue_number_min + 1 + residue_number_offset
+        )
+        residue_number_last = g["residue_number"].max()
+        chains[i] = g
+
+    return pd.concat(chains).reset_index(drop=True)
 
 
 def main(args):
@@ -48,12 +71,11 @@ def main(args):
     print(f"# items in chain_idx: {len(chain_group_dict)}")
 
     for pdb_file in tqdm(args.pdb_file):
-        df = read_pdb(path=pdb_file)
+        df_pdb = read_pdb(path=pdb_file)
         for i, chain_selection in enumerate(
-            chain_group_dict.get(pdb_file.stem, [df["chain_id"].unique().tolist()])
+            chain_group_dict.get(pdb_file.stem, [df_pdb["chain_id"].unique().tolist()])
         ):
-            if chain_selection:
-                df = select_chains(df, chain_selection)
+            df = select_chains(df_pdb, chain_selection)
 
             # do chain permutations
             chain_selection_list = list(
@@ -64,12 +86,11 @@ def main(args):
             random.shuffle(chain_selection_list)
 
             for chain_selection in chain_selection_list[:args.topk_chain_permutation]:
-                df = df.sort_values(
-                    by="chain_id",
-                    key=lambda col: col.map(
-                        {c : i for i, c in enumerate(chain_selection)}
-                    )
-                ).reset_index(drop=True)
+                # rearrange chains & residue_number
+                df = rearrange_pdb(
+                    df, chain_selection, pseudo_linker_length=args.pseudo_linker_length
+                )
+
                 chain_selection = "".join(chain_selection)
                 save_pdb(
                     args.output_dir / f"{pdb_file.stem}_{chain_selection}.pdb", df
@@ -93,6 +114,12 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--topk_chain_permutation", type=int, default=None, help="topk chain permutation"
+    )
+    parser.add_argument(
+        "--pseudo_linker_length",
+        type=int,
+        default=128,
+        help="add a pseudo linker between chains"
     )
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
 
